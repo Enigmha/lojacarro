b1f93417a63060246167d317b4ecfa42dd58a5cd
Leandro Cabral <enigmha@gmail.com>
2021-12-27 13:04:28 -0300

    Criando projeto

---
+++ backend/src/main/java/com/lojadecarro/lojacarro/LojacarroApplication.java
@@ -0,0 +1,13 @@
+package com.lojadecarro.lojacarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojacarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojacarroApplication.class, args);
+	}
+
+}
